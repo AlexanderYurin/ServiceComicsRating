ce09aadc3ebc62c15a73b3eac200f7062d8e5c8f
Alexander Yurin <hiimmandarin32@gmail.com>
2024-03-19 12:22:38 +0300

    Добавлены serializers.py

--- app/serializers.py
+++ app/serializers.py
@@ -1,9 +1,17 @@
-from django.db.migrations import serializer
+from django.core.validators import MinValueValidator, MaxValueValidator
+from rest_framework import serializers
 
 from app.models import Comic
 
 
-class ComicRatingSerializer(serializer.ModelFieldSerializer):
+class ComicSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Comic
-		fields = ("__all__",)
+		fields = "__all__"
+
+
+class RatingSerializer(serializers.Serializer):
+	comic_id = serializers.IntegerField()
+	user_id = serializers.IntegerField()
+	value = serializers.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
